8bcbd54af774ad260f50bef4794ffd4f0a5e2451
avinash1246 <85864796+avinash1246@users.noreply.github.com>
2024-03-20 11:13:52 +0530

    Update imageUtils.java

--- zezha-backEnd/zezha/src/main/java/com/zezha/works/utility/imageUtils.java
+++ zezha-backEnd/zezha/src/main/java/com/zezha/works/utility/imageUtils.java
@@ -1,8 +1,18 @@
 package com.zezha.works.utility;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.util.Hashtable;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class imageUtils {
 
@@ -41,6 +51,32 @@ public class imageUtils {
 			
 		}
 		return outStream.toByteArray();
+	}
+	
+	public static String generateQRCodeBase64(String link) {
+		int width = 500;
+		int height = 500;
+		String format = "png";
+		Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<>();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		BitMatrix bitMatrix;
+		try {
+			bitMatrix = new MultiFormatWriter().encode(link, BarcodeFormat.QR_CODE, width, height, hintMap);
+
+			BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			for (int x = 0; x < width; x++)
+				for (int y = 0; y < height; y++)
+					image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(image, format, baos);
+			byte[] imageData = baos.toByteArray();
+
+			return "data:image/png;base64," + java.util.Base64.getEncoder().encodeToString(imageData);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 }
